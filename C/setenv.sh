9a2d9a09a9370ac6d7a57f7277707304d8a7a91f
hanshaoyang <hanshaoyang@onething.net>
2019-03-11 20:35:07 +0800

    null

--- C/setenv.sh
+++ C/setenv.sh
@@ -1,47 +1,5 @@
 #!/bin/bash 
-
-download_toolchain()
-{
-    if [ ! -d ./toolchain ];then
-        echo "Downloading toolchain from gitlab"
-        git clone git@wx-gitlab.xunlei.cn:FirmwareSupport/toolchain.git
-    fi
-}
-
-
-export_env_ws1508()
-{
-    export ROOT=`pwd`
-    export PRODUCT_ID='WS1508'
-    export STAGING_DIR="/opt/staging_dir/target-arm_cortex-a5+neon_eglibc-2.19_eabi"
-    CROSS_TOOLCHAINS_DIR="/opt/staging_dir/toolchain-arm_cortex-a5+neon_gcc-4.8-linaro_eglibc-2.19_eabi"
-    
-    if [ ! -h "/opt/staging_dir" ];then
-        if [ ! -x "${ROOT}/toolchain/staging_dir" ];then
-            cd ${ROOT}
-            download_toolchain
-            cd $ROOT/toolchain
-            tar xjvf staging_dir.tar.bz2 
-        fi
-
-        echo "Running command : sudo ln -s ${ROOT}/toolchain/staging_dir /opt/staging_dir"
-        echo "Please input the root password,and continue"
-        sudo ln -s ${ROOT}/toolchain/staging_dir /opt/staging_dir
-        echo "Create soft link /opt/staging_dir success"
-    fi
-
-   
-    export PATH=$PATH:${CROSS_TOOLCHAINS_DIR}/bin
-    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CROSS_TOOLCHAINS_DIR/usr/lib:$ROOT/build/lib
-    export NAND_FLASH_SUPPORT=1
-    export AMLOGIC_PLATFORM=1
-    export CROSS_COMPILE=arm-openwrt-linux-gnueabi-
-    export TOOLCHAIN_ROOT="/opt/staging_dir/target-arm_cortex-a5+neon_eglibc-2.19_eabi"
-    export REPO_NAME=zqb_ipk
-    cd $ROOT
-}
-
-_export_env_ws1508()
+export_env_aarch64()
 {
     export ROOT=`pwd`
     export PRODUCT_ID='WS1508'
@@ -70,7 +28,7 @@ _export_env_ws1508()
     cd $ROOT
 }
 
-export_env_ws1608()
+export_env_arm()
 {
     export ROOT=`pwd`
     export PRODUCT_ID='WS1608'
@@ -121,16 +79,16 @@ fi
 
 case $1 in
 "")
-    echo product is ws1408
-    export_env_ws1608
+    echo product is x86
+    export_env_x86
     ;;
 1)
-    echo product is minecrafter pro
-    export_env_ws1508
+    echo product is arm
+    export_env_arm
     ;;
 2)
-    echo product is onecloud
-    export_env_ws1608
+    echo product is aarch64
+    export_env_aarch64
     ;;
 *)
     echo unsupport product id 
